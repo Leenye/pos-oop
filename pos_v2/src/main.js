7a3c7d37021b4894622dcf3a584333742cf00c71
lyj <liyujing1990X@outlook.com>
2014-11-06 23:02:37 +0800

    add parsePurchasedItems function

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -1,8 +1,12 @@
 //TODO: Please write code in this file.
 
 function printInventory(inputs) {
-
     printTime();
+    // for(var i=0; i<inputs.length, i++){
+    //     isWeightedFood(inputs[i]);
+    //     parseSymbol(inputs[i]);
+    // }
+  // //  var purchasedItems = parsePurchasedItems(inputs);
     console.log(formattedDateString);
 }
 
@@ -10,21 +14,20 @@ function printTime(){
     dateDigitToString = function (num) {
         return num < 10 ? '0' + num : num;
     };
-   var currentDate = new Date(),
-   year = dateDigitToString(currentDate.getFullYear());
-   month = dateDigitToString(currentDate.getMonth() + 1),
-   date = dateDigitToString(currentDate.getDate()),
-   hour = dateDigitToString(currentDate.getHours()),
-   minute = dateDigitToString(currentDate.getMinutes()),
-   second = dateDigitToString(currentDate.getSeconds()),
-   formattedDateString = year + '年' + month + '月' + date + '日 ' + hour + ':' + minute + ':' + second;
-   return formattedDateString;
+    var currentDate = new Date(), //why the punctuation must be the comma ?
+    year = dateDigitToString(currentDate.getFullYear());
+    month = dateDigitToString(currentDate.getMonth() + 1),
+    date = dateDigitToString(currentDate.getDate()),
+    hour = dateDigitToString(currentDate.getHours()),
+    minute = dateDigitToString(currentDate.getMinutes()),
+    second = dateDigitToString(currentDate.getSeconds()),
+    formattedDateString = year + '年' + month + '月' + date + '日 ' + hour + ':' + minute + ':' + second;
+    return formattedDateString;
 }
 
-var input = 'ITEM000003-2';
-
 function isWeightedFood(input) {
-    return input.indexOf('-') != -1;
+
+     return input.indexOf('-') != -1 ;
 }
 
 function parseSymbol(input) {
@@ -33,34 +36,38 @@ function parseSymbol(input) {
     }
 }
 
-var allPromotions = loadAllPromotions;
+//var allPromotions = loadAllPromotions;
+
 // function isPromotion(input){
 //     var x;
 //      //problem below: how to reference an array in an object
-//     for(x in allPromotions[0].['ITEM000000','ITEM000001','ITEM000005']){
-//          if (input == x)
+//     for(x in allPromotions.['ITEM000000','ITEM000001','ITEM000005']){
+//          if (input == x){
 //            return (1);
+//          }
+//          else{
+//            rerurn (0);
+//          }
 //     }
 // }
 
-function PurchasedItem(barcode, name ,unit ,price ,amount ){
-    this.barcode = barcode;
-    this.name = name;
-    this.unit = unit;
-    this.price = price;
-    this.amout = amount;
-}
+function parsePurchasedItems(inputs){
+    var purchasedItems = {};
+    for (var i=0; i <inputs.length, i++){
+        var barCode = inputs[i];
+        var itemAmount = 1;
 
-//problem below: get the purchased item and amount.but it doesn't work
-PurchasedItems.prototype.getItemAndAmount = function(input){
-    if(isWeightedFood(input)){
-        return (new PurchasedItem(parseSymbol(input)[0], '','',0,parseSymbol(input)[1]));
+        if (isWeightedFood(inputs[i])) {
+            barCode = parseSymbol(inputs[i]][0];
+            itemAmount = parseSymbol(inputs[i]][1];
+        }
+
+        if (purchasedItems.hasOwnProperty(barcode)){
+            purchasedItems[barCode] += itemAmount;
+        }
+        else{
+            purchasedItems[barCode] = itemAmount;
+        }
     }
-    else if(PurchasedItems.hasOwnProperty.barcode != input){
-        return (new PurchasedItem(input, '','',0,1));
-    }
-    else {
-        this.amount += 1;
-        return (PurchasedItem(input, '','', 0,this.amount));
-    }
+    return purchasedItems;
 }
