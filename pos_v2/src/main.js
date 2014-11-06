66c3cf5176b8656e6335e571f10b39899e346282
lyj <liyujing1990X@outlook.com>
2014-11-06 21:13:28 +0800

    time encapsulated

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -2,19 +2,23 @@
 
 function printInventory(inputs) {
 
+    printTime();
+    console.log(formattedDateString);
+}
 
-  dateDigitToString = function (num) {
-              return num < 10 ? '0' + num : num;
-  }
-  var currentDate = new Date(),
-            year = dateDigitToString(currentDate.getFullYear()),
-            month = dateDigitToString(currentDate.getMonth() + 1),
-            date = dateDigitToString(currentDate.getDate()),
-            hour = dateDigitToString(currentDate.getHours()),
-            minute = dateDigitToString(currentDate.getMinutes()),
-            second = dateDigitToString(currentDate.getSeconds()),
-            formattedDateString = year + '年' + month + '月' + date + '日 ' + hour + ':' + minute + ':' + second;
-      console.log(formattedDateString);
+function printTime(){
+    dateDigitToString = function (num) {
+        return num < 10 ? '0' + num : num;
+    };
+   var currentDate = new Date(),
+   year = dateDigitToString(currentDate.getFullYear());
+   month = dateDigitToString(currentDate.getMonth() + 1),
+   date = dateDigitToString(currentDate.getDate()),
+   hour = dateDigitToString(currentDate.getHours()),
+   minute = dateDigitToString(currentDate.getMinutes()),
+   second = dateDigitToString(currentDate.getSeconds()),
+   formattedDateString = year + '年' + month + '月' + date + '日 ' + hour + ':' + minute + ':' + second;
+   return formattedDateString;
 }
 
 var input = 'ITEM000003-2';
