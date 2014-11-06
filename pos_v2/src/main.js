ba671528e5927c0d8d69a0a4a1b9376b57a937a4
lyj <liyujing1990X@outlook.com>
2014-11-06 20:37:12 +0800

    add the time

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -2,16 +2,19 @@
 
 function printInventory(inputs) {
 
-  //  for(var i=0; i < inputs.length; i++) {
-      //  console.log(isPromotion(inputs[i]));
-      // console.log(parseSymbol(inputs[i]));
 
-  //  }
-
-  console.log(parseSymbol(input));
-  //problem below: how to reference an object in a function
-  console.log(PurchasedItem);
-
+  dateDigitToString = function (num) {
+              return num < 10 ? '0' + num : num;
+  }
+  var currentDate = new Date(),
+            year = dateDigitToString(currentDate.getFullYear()),
+            month = dateDigitToString(currentDate.getMonth() + 1),
+            date = dateDigitToString(currentDate.getDate()),
+            hour = dateDigitToString(currentDate.getHours()),
+            minute = dateDigitToString(currentDate.getMinutes()),
+            second = dateDigitToString(currentDate.getSeconds()),
+            formattedDateString = year + '年' + month + '月' + date + '日 ' + hour + ':' + minute + ':' + second;
+      console.log(formattedDateString);
 }
 
 var input = 'ITEM000003-2';
