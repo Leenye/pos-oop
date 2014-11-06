d5e53b0c1d41ff0f0743d3372ad2bab4b9e9ea93
lyj <liyujing1990X@outlook.com>
2014-11-06 17:55:49 +0800

    more problems

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -2,11 +2,19 @@
 
 function printInventory(inputs) {
 
-    for(var i=0; i < inputs.length; i++) {
-        console.log(isPromotion(inputs[i]));
+  //  for(var i=0; i < inputs.length; i++) {
+      //  console.log(isPromotion(inputs[i]));
+      // console.log(parseSymbol(inputs[i]));
 
-    }
+  //  }
+
+  console.log(parseSymbol(input));
+  //problem below: how to reference an object in a function
+  console.log(PurchasedItem);
+
 }
+
+var input = 'ITEM000003-2';
 
 function isWeightedFood(input) {
     return input.indexOf('-') != -1;
@@ -19,20 +27,33 @@ function parseSymbol(input) {
 }
 
 var allPromotions = loadAllPromotions;
+// function isPromotion(input){
+//     var x;
+//      //problem below: how to reference an array in an object
+//     for(x in allPromotions[0].['ITEM000000','ITEM000001','ITEM000005']){
+//          if (input == x)
+//            return (1);
+//     }
+// }
 
-function isPromotion(input){
-    var x;
-    //problem below: how to reference an array in an object
-    for(x in allPromotions[0].['ITEM000000','ITEM000001','ITEM000005']){
-        if (input == x)
-          return (1);
-    }
-}
-
-function PurcasedItem(barcode, name ,unit ,price ,amount ){
+function PurchasedItem(barcode, name ,unit ,price ,amount ){
     this.barcode = barcode;
     this.name = name;
     this.unit = unit;
     this.price = price;
     this.amout = amount;
+}
+
+//problem below: get the purchased item and amount.but it doesn't work
+PurchasedItems.prototype.getItemAndAmount = function(input){
+    if(isWeightedFood(input)){
+        return (new PurchasedItem(parseSymbol(input)[0], '','',0,parseSymbol(input)[1]));
+    }
+    else if(PurchasedItems.hasOwnProperty.barcode != input){
+        return (new PurchasedItem(input, '','',0,1));
+    }
+    else {
+        this.amount += 1;
+        return (PurchasedItem(input, '','', 0,this.amount));
+    }
 }
